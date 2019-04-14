023d26242fe56350e7575fbfd06094edcfdda442
Calvin Dao <calvindao16@gmail.com>
2019-04-14 17:27:30 -0500

    Finished minimal solution

--- processor.py
+++ processor.py
@@ -1,29 +1,34 @@
 #find all .txt/word doc files in folder
 import os, glob
 import numpy as np
-filetype = "*.txt"
+import pandas as pd
+import xlsxwriter as xwriter
 
+
+#Settings
+filetype = "*.txt"
 targs = glob.glob(filetype)
 
 # processes the files of extraneous data
 
-for i in targs:
-    f = open(i, "r")
-    lines = f.readlines()
+for i in targs:                     #for every file
+    f = open(i, "r")                #open it in a read only state
+    lines = f.readlines()           #read the lines and store
     f.close()
 
-    temp = i[0: len(i)-3] + "t"
-    f = open(temp, "w")
+    temp = i[0: len(i)-3] + "t"     #The output file removes .txt and appends .t 
+    f = open(temp, "w")             #the temp files are the original file, minus the metadata
     for line in lines:
         if not ">>>>>" in line:
             f.write(line)
     f.close()
 #
-targs = glob.glob("*.t")
+targs = glob.glob("*.t") # look over all the temp files made
 
-tArray = np.loadtxt(targs[0], dtype = float)
-tArray = tArray.tolist()
+tArray = np.loadtxt(targs[0], dtype = float) #have numpy process them
+tArray = tArray.tolist() # turn the first input into a list
 
+#sort all the data points into their slots
 for i in range(1, len(targs)):
     t2Array = np.loadtxt(targs[i], dtype = float).tolist()
     for i in range(len(t2Array)):
@@ -32,4 +37,10 @@ a = ["Wave Length"]
 for i in range(len(tArray[0])-1):
     a.append(i)
 tArray = [a] + tArray
-print(tArray)
+
+workbook = xwriter.Workbook('output.xlsx')
+worksheet = workbook.add_worksheet()
+
+for row, data in enumerate(tArray):
+    worksheet.write_row(row, 0, data)
+workbook.close()
